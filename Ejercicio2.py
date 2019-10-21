fc5da2c3ab2e6a0ef86b20057c3e4bb1d70897e0
Genesis Ramon <genesis.ramon@usil.pe>
2019-10-21 09:17:41 -0500

    Resolucion ejercicio 2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,14 +1,1 @@
-#Hacer que el sistema genere un numero aleatorio entre 1 y 10. Luego hacer que el usuario
-#adivine el número. La aplicación debe terminar cuando el usuario adivine.
-import random 
-
-
-n= random.randint(1,10)
-while True:
-    a= int(input("Adivine número del sistema (1,10)"))
-    if n==a:
-        print("Adivinaste!")
-        break
-    else:
-        print("Numero incorrecto")
-
+import random
